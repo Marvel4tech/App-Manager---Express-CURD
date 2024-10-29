b25347f06ace06dfbb5ac5af8c5f13d4d86f49ed
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-29 06:56:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const dotenv = require("dotenv").config()
+const app = express()
+
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server running on port ${PORT}`)
+})
