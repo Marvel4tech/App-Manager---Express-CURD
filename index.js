249e128559a6976ebe5a5d20d7c11515d70b417a
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-29 09:47:43 +0100

    Error middleware Handler added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const dotenv = require("dotenv").config()
 const app = express()
 const contactRoute = require("./routes/contactRoutes")
+const errorHandler = require("./middleware/errorHandler")
 
 app.use(express.json())
 
 app.use("/api/contacts", contactRoute)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
