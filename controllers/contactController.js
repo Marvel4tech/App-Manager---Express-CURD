817410bca5c6daceddef0efc1c63e36b70385dbd
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-29 08:29:57 +0100

    contact controller for contacts CRUD operation added

---
+++ controllers/contactController.js
@@ -0,0 +1,48 @@
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access Public
+const getContacts = (req, res) => {
+    res.status(200).json({ message: "Get all the contacts" })
+}
+
+//@desc Post new contact
+//@route POST /api/contacts
+//@access Public
+const createContact = (req, res) => {
+    res.status(201).json({ message: "Create new contact" })
+}
+
+//@desc Get single contact
+//@route GET /api/contacts/:id
+//@access Public
+const getContact = (req, res) => {
+    res.status(200).json({message: `Get contact for ${req.params.id}`})
+}
+
+//@desc update contact
+//@route PUT /api/contacts/:id
+//@access Public
+const updateContact = (req, res) => {
+    res.status(200).json({message: `Update contact for ${req.params.id}`})
+}
+
+//@desc delete new contact
+//@route DELETE /api/contacts/:id
+//@access Public
+const deleteContact = (req, res) => {
+    res.status(200).json({message: `Delete contact for ${req.params.id}`})
+}
+
+
+
+
+
+module.exports = {
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
+}
+
+
