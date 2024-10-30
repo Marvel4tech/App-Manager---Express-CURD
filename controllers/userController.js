f6ea0b9c3af0d706cde8dd36133144eefb27864c
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-30 11:03:57 +0100

    userController is added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,28 @@
-const register = (req, res) => {
-    res.status(200).json({ message: "Welcome to resgister page" })
-}
+const asyncHandler = require("express-async-handler")
 
-const login = (req, res) => {
-    res.status(200).json({ message: "Welcome to login page" })
-}
+//@desc Register user
+//@route POST /api/users/register
+//@access Public
+const registerUser = asyncHandler (async (req, res) => {
+    res.status(200).json({ message: "register new user" })
+})
 
-module.exports = { register, login }
+//@desc Login user
+//@route POST /api/users/login
+//@access Public
+const loginUser = asyncHandler (async (req, res) => {
+    res.status(200).json({ message: "login user" })
+})
+
+//@desc current user information
+//@route POST /api/users/current
+//@access Private
+const currentUser = asyncHandler (async (req, res) => {
+    res.status(200).json({ message: "current user information" })
+})
+
+module.exports = {
+    registerUser,
+    loginUser,
+    currentUser,
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,11 @@
 const express = require("express")
+const { registerUser, loginUser, currentUser } = require("../controllers/userController")
 const router = express.Router()
 
-router.post("/register", (req, res) => {
-    res.status(200).json({ message: "register new user" })
-})
+router.post("/register", registerUser)
 
-router.post("/login", (req, res) => {
-    res.status(200).json({ message: "login user" })
-})
+router.post("/login", loginUser)
 
-router.get("/current", (req, res) => {
-    res.status(200).json({ message: "current user information" })
-})
+router.get("/current", currentUser)
 
 module.exports = router;
